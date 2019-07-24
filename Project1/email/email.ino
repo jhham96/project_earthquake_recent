f3b80f6f9a81cce772695b86ad752f862c9b84bb
jhham96 <logbug1004@gmail.com>
2019-07-25 05:41:48 +0900

    LCD 제거

--- Project1/email/email.ino
+++ Project1/email/email.ino
@@ -1,6 +1,10 @@
 #include <mp3.h>      // mp3 library
 #include <email.h>    // for email library
 
+// #include <LiquidCrystal_I2C.h>     //LiquidCrystal 라이브러리 추가
+
+
+// LiquidCrystal_I2C lcd(0x27, 16, 2);  //lcd 객체 선언
 
 char data;
 
@@ -12,6 +16,13 @@ void setup() {
   setup_email();    // email code setup
   Serial.println("MP3 setup 실시");
   setup_mp3();      // mp3 code for setup
+
+  // lcd.begin(); //LCD 사용 시작
+  //
+  // lcd.clear();
+  // lcd.setCursor(0, 0);    // 커서를 0, 0에 가져다 놓아라. (열, 행)
+  // lcd.print("1. sensor");     // 출력
+
 }
 
 void loop() {
@@ -26,17 +37,17 @@ void loop() {
     // 공통적으로 해야하는 것들
     // 1. email 보내기
     loop_email(data);
-  
+
     // 2. MP3로 음성안내
     Serial.println("MP3 음성안내 시작");
     while(loop_mp3())
       ;
-  
+
     delay(60000);
     Serial.println("music finish");
 
     // data 초기화
-    data = 0;  
+    data = 0;
   }
-  
+
 }
